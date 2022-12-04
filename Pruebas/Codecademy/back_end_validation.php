8cc544fcf2b3cab096bbc616113f3242d9d4669a
PaulaMH <paulamorenohermoso99@gmail.com>
2022-12-04 12:20:34 +0100

    Modificacion

--- Pruebas/Codecademy/back_end_validation.php
+++ Pruebas/Codecademy/back_end_validation.php
@@ -1,29 +1,28 @@
 <?php
-$users = ["coolBro123" => "password123!", "coderKid" => "pa55w0rd*", "dogWalker" => "ais1eofdog$"];  
-  
-  
+$users = ["coolBro123" => "password123!", "coderKid" => "pa55w0rd*", "dogWalker" => "ais1eofdog$"];
+
+
 $feedback = "";
 $message = "You're logged in!";
 $validation_error = "* Incorrect username or password.";
 $username = "";
 
+
 // Write your code here:
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $username = $_POST["username"];
   $password = $_POST["password"];
 
+  //Para comprobar si existe tal usuario
   //De esta manera puedo recorrer el array asociativo $users
   //sin tener que hacer un bucle!!!
-  if(isset($users[$username]))
-  {
-    if($users[$username] == $password)
-    {
+  if (isset($users[$username])) {
+    if ($users[$username] == $password) {
       $feedback = $message;
-    }else{
+    } else {
       $feedback = $validation_error;
     }
-  }else{
+  } else {
     $feedback = $validation_error;
   }
 }
@@ -35,13 +34,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 
 
 ?>
-  
+
 <h3>Welcome back!</h3>
 <form method="post" action="">
-Username:<input type="text" name="username" value="<?php echo $username;?>">
-<br>
-Password:<input type="text" name="password" value="">
-<br>
-<input type="submit" value="Log in">
+  Username:<input type="text" name="username" value="<?php echo $username; ?>">
+  <br>
+  Password:<input type="text" name="password" value="">
+  <br>
+  <input type="submit" value="Log in">
 </form>
-<span class="feedback"><?= $feedback;?></span>
+<span class="feedback"><?= $feedback; ?></span>
